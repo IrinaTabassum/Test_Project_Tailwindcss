496fbea0e36f0884a39c93eac3c734fbc3243916
Irina <tabassum15-553@diu.edu.bd>
2022-07-30 17:57:57 +0600

    add all files

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        mainBackcolor: "#E5E5E5",
+        darkgreen:"#647B64",
+        graGreenDark:"#698469",
+        graGreenLight:"#66AB66",
+
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
